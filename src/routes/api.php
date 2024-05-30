f05f79f3d5e5870c1b8dcc19476d25df590c585d
koutorino <tako.omg@gmail.com>
2024-05-30 22:56:00 +0900

    add: APIに必要なファイルの追加

--- src/routes/api.php
+++ src/routes/api.php
@@ -17,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['middleware' => 'api'])->group(function () {
+    # 投稿作成
+    Route::post('/posts/create', 'PostController@create');
+    # 投稿一覧表示
+    Route::get('/posts', 'PostController@index');
+    # 投稿表示
+    Route::get('/posts/{id}', 'PostController@show');
+    # 投稿編集
+    Route::patch('/posts/update/{id}' , 'PostController@update');
+    # 投稿削除
+    Route::delete('/posts/{id}', 'PostController@delete');
+});
