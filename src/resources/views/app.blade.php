2b128d890d5a0cccaba5472cd7f5063d0745bf80
koutorino <tako.omg@gmail.com>
2024-06-01 13:56:11 +0900

    .ignoreファイルの適用

---
+++ src/resources/views/app.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @viteReactRefresh
+    @vite(['resources/scss/app.scss', 'resources/ts/app.ts'])
+</head>
+<body>
+    <div id="app">
+    </div>
+
+    {{ Storage::disk('s3')->put('test.txt', 'test') }}
+    {{ Storage::disk('s3')->get('test.txt'); }}
+</body>
+</html>
